36e6d98c100018a824288f4f909b005e0f1099ea
SaidAMG <113219527+SaidAMG@users.noreply.github.com>
2023-10-21 20:16:14 +0200

    Dal aangemaakt, Booklet Crud Aagemaakt, Booklet Crud getest

---
+++ DAL.cs
@@ -0,0 +1,113 @@
+﻿using MBPC_VeilingApp.Classes;
+using System.Data.SqlClient;
+
+namespace MBPC_VeilingApp
+{
+    public static class DAL
+    {
+        static string connectionString = "Data Source=.;Initial Catalog=MBPC;Integrated Security=True";
+        public static List<Booklet> booklets = new List<Booklet>();
+
+
+        public static void RefreshDAL()
+        {
+            // hier alle reads
+            ReadBooklets();
+            //memerread
+            //Auction
+            //lot
+            //lot2mem
+        }
+
+        //CRUD Booklet
+        public static void CreateBooklet(Booklet _booklet)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string qry = "INSERT INTO BOOKLET(name, pane, description) VALUES(@name, @pane, @description)";
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(qry, connection))
+                {
+                    //command.Parameters.AddWithValue("@bookletID",_booklet.GetId());
+                    command.Parameters.AddWithValue("@name", _booklet.GetName());
+                    command.Parameters.AddWithValue("@pane",_booklet.GetPane());
+                    command.Parameters.AddWithValue("@description",_booklet.GetDescription());
+                    command.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+
+        public static void ReadBooklets()
+        {
+            booklets.Clear();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string qry = "SELECT bookletID, name, pane, description FROM BOOKLET";
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(qry, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Booklet booklet = new Booklet(
+                                (int)reader[0], 
+                                (string)reader[1], 
+                                (string)reader[2], 
+                                (string)reader[3]);
+                            
+                            booklets.Add(booklet);
+                        }
+                    }
+                }
+                connection.Close();
+            }
+        }
+
+        public static void UpdateBooklet(Booklet _booklet)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string qry = "UPDATE BOOKLET SET name = @name, pane = @pane, description = @description WHERE bookletID = @bookletID";
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(qry, connection))
+                {
+                    command.Parameters.AddWithValue("@bookletID",_booklet.GetId());
+                    command.Parameters.AddWithValue("@name", _booklet.GetName());
+                    command.Parameters.AddWithValue("@pane", _booklet.GetPane());
+                    command.Parameters.AddWithValue("@description", _booklet.GetDescription());
+                    command.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+
+        public static void DeleteBooklet(Booklet _booklet)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string qry = "DELETE BOOKLET WHERE bookletID = @bookletID";
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(qry, connection))
+                {
+                    command.Parameters.AddWithValue("@bookletID", _booklet.GetId());
+                    command.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+
+        //CRUD Member
+
+
+        //CRUD Lot
+
+
+        //CRUD Aution
+
+
+        //CRUD Lot2Mem
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,30 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// said was here
-// kane was here
-// Dennis was here
-// Jeroen was here
+﻿using MBPC_VeilingApp;
+using MBPC_VeilingApp.Classes;
 
 
-void test()
-{
+//----------------------------------------------------------------------------------------------------> Voorbeeld van Saïd
+//Booklet booklet = new Booklet(284,"test2","roodkapje1","STILL THE GOAT");
 
-}
+//DAL.CreateBooklet(booklet);
+//DAL.UpdateBooklet(booklet);
+//DAL.DeleteBooklet(booklet);
+//DAL.ReadBooklets();
 
-void test2()
-{
+//List<Booklet> booklets = DAL.booklets;
 
-}
+//foreach(Booklet b in booklets)
+//{
+//    Console.WriteLine($"{b.GetId()},{b.GetName()},{b.GetPane()},{b.GetDescription()}");
+//}
+//----------------------------------------------------------------------------------------------------> Voorbeeld van Saïd
 
-Console.WriteLine("   O        ");
-Console.WriteLine("  /|\\       ");
-Console.WriteLine("  / \\       ");
-Console.WriteLine(" /___\\      ");
-Console.WriteLine("  | |       ");
-Console.WriteLine("  | |       ");
-Console.WriteLine(" /   \\      ");
-Console.WriteLine("|     |     ");
-Console.WriteLine("=======");
-Console.WriteLine("  SAID      ");
-Console.WriteLine("   ^        ");
-Console.WriteLine("   |        ");
+
+//----------------------------------------------------------------------------------------------------> Voorbeeld van Dennis
+
+// implementatie 
+
+//----------------------------------------------------------------------------------------------------> Voorbeeld van Dennis
+
+//----------------------------------------------------------------------------------------------------> Voorbeeld van Jeroen
+
+// implementatie 
+
+//----------------------------------------------------------------------------------------------------> Voorbeeld van Jeroen
+
+//----------------------------------------------------------------------------------------------------> Voorbeeld van Kane
+
+// implementatie 
+
+//----------------------------------------------------------------------------------------------------> Voorbeeld van Kane
+
+
+
+//----------------------------------------------------------------------------------------------------> Implementatie veilinglijs aanmaken
+
+// implementatie
+
+//----------------------------------------------------------------------------------------------------> Implementatie veilinglijs aanmaken
