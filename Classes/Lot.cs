5558b70d8dda634f2f8c73a8fa09a6024521ae23
Jeroen Stekelenburg <1747142stekelenburg@zuyd.nl>
2023-10-23 11:47:32 +0200

    Aanmaken class Lot. Toewijzen datatypen aan variabelen. beging constructors voor class Lot

---
+++ Classes/Lot.cs
@@ -0,0 +1,26 @@
+﻿namespace MBPC_VeilingApp.Classes
+using System;
+
+public class Lot
+{
+	public Lot()
+	{
+	private int id;
+	private int auctionId;
+	private int vendorId;
+	private int bookletId;
+	private int lotId;
+	private string memberReference;
+	private string description;
+	private string perfType;
+	private string perfCondition;
+	private bool verified;
+	private double reservePrice;
+
+	public Lot(int _id,int _auctionId, int _vendorId,int _bookletId, int _lotId, string _memberReference, string _description, string _perfType, string _perfCondition, bool _verified, double _reservePrice)
+	{
+
+	}
+
+	}
+}
