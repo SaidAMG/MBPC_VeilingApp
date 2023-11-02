16795de8ff7e8ef0c06664b12c3118acb920b1ce
Jeroen Stekelenburg <1747142stekelenburg@zuyd.nl>
2023-11-02 12:50:13 +0100

    class Sort werkend class CustomComparer toegevoegd als nested class in class Sort

--- Classes/Sort.cs
+++ Classes/Sort.cs
@@ -1,141 +1,74 @@
-﻿using System.Text.RegularExpressions;
+﻿using MBPC_VeilingApp;
+using System.Text.RegularExpressions;
 
 namespace MBPC_VeilingApp.Classes
 {
     internal class Sort
     {
-        public class LotSorter : IComparer<List<object>>
+        private class CustomComparer : IComparer<List<object>>
+        {
+            public int Compare(List<object> x, List<object> y)
             {
-                public int Compare(List<object> x, List<object> y)
+                for (int i = 0; i < Math.Min(x.Count, y.Count); i++)
                 {
-                    for (int i = 0; i < Math.Min(x.Count, y.Count); i++)
+                    if (x[i] is int && y[i] is int)
                     {
-                        if (x[i] is int && y[i] is int)
+                        int numX = (int)x[i];
+                        int numY = (int)y[i];
+
+                        int result = numX.CompareTo(numY);
+
+                        if (result != 0)
                         {
-                            int numX = (int)x[i];
-                            int numY = (int)y[i];
-
-                            int result = numX.CompareTo(numY);
-
-                            if (result != 0)
-                            {
-                                return result;
-                            }
-                        }
-                        else
-                        {
-                            string strX = x[i].ToString();
-                            string strY = y[i].ToString();
-
-                            int result = strX.CompareTo(strY);
-
-                            if (result != 0)
-                            {
-                                return result;
-                            }
+                            return result;
                         }
                     }
+                    else
+                    {
+                        string strX = x[i].ToString();
+                        string strY = y[i].ToString();
 
-                    return x.Count.CompareTo(y.Count);
+                        int result = strX.CompareTo(strY);
+
+                        if (result != 0)
+                        {
+                            return result;
+                        }
+                    }
                 }
-            }
 
-            public List<Lot> SortLotsByAuctionIdAndBookletName(List<Lot> lots)
-            {
-                List<Lot> sortedLotsByAuctionId = lots.OrderBy(lot => lot.GetAuctionId().GetId()).ToList();
-                List<Lot> sortedLotsByName = sortedLotsByAuctionId.OrderBy(lot =>
-                {
-                    string bookletName = lot.GetBookletId().GetName();
-                    List<string> parts = Regex.Split(bookletName, @"(?<=[0-9])(?=[A-Za-z])").ToList();
-                    List<object> parsedParts = new List<object>();
-
-                    foreach (string part in parts)
-                    {
-                        if (int.TryParse(part, out int number))
-                        {
-                            parsedParts.Add(number);
-                        }
-                        else
-                        {
-                            parsedParts.Add(part);
-                        }
-                    }
-
-                    return parsedParts;
-                }, new LotSorter()).ToList();
-
-                return sortedLotsByName;
+                return x.Count.CompareTo(y.Count);
             }
         }
+
+        public List<Lot> SortLotsByAuctionIdAndBookletName(List<Lot> lots)
+        {
+            List<Lot> sortedLotsByName = lots;
+            List<Lot> sorted = sortedLotsByName.OrderBy(lot =>
+            {
+                string bookletName = lot.GetBookletId().GetName();
+                List<string> parts = Regex.Split(bookletName, @"(?<=[0-9])(?=[A-Za-z])").ToList();
+                List<object> parsedParts = new List<object>();
+
+                foreach (string part in parts)
+                {
+                    if (int.TryParse(part, out int number))
+                    {
+                        parsedParts.Add(number);
+                    }
+                    else
+                    {
+                        parsedParts.Add(part);
+                    }
+                }
+
+                return parsedParts;
+            }, new CustomComparer()).ToList();
+
+            return sortedLotsByName;
+        }
     }
-//    private class CustomComparer : IComparer<List<object>>
-//    {
-//        public int Compare(List<object> x, List<object> y)
-//        {
-//            for (int i = 0; i < Math.Min(x.Count, y.Count); i++)
-//            {
-//                if (x[i] is int && y[i] is int)
-//                {
-//                    int numX = (int)x[i];
-//                    int numY = (int)y[i];
+}
 
-//                    int result = numX.CompareTo(numY);
 
-//                    if (result != 0)
-//                    {
-//                        return result;
-//                    }
-//                }
-//                else
-//                {
-//                    string strX = x[i].ToString();
-//                    string strY = y[i].ToString();
-
-//                    int result = strX.CompareTo(strY);
-
-//                    if (result != 0)
-//                    {
-//                        return result;
-//                    }
-//                }
-//            }
-
-//            return x.Count.CompareTo(y.Count);
-//        }
-//    }
-//    public List<Lot> SortLotsByAuctionIdAndBookletName(List<Lot> lots)
-//    {
-//        List<Lot> sortedLotsByName = lots;
-//        List<Lot> sorted = sortedLotsByName.OrderBy(lot =>
-//        {
-//            string bookletName = lot.GetBookletId().GetName();
-//            List<string> parts = Regex.Split(bookletName, @"(?<=[0-9])(?=[A-Za-z])").ToList();
-//            List<object> parsedParts = new List<object>();
-
-//            foreach (string part in parts)
-//            {
-//                if (int.TryParse(part, out int number))
-//                {
-//                    parsedParts.Add(number);
-//                }
-//                else
-//                {
-//                    parsedParts.Add(part);
-//                }
-//            }
-
-//            return parsedParts;
-//        }, new CustomComparer()).ToList();
-
-//        return sortedLotsByName;
-//    }
-//}
-//public List<Lot> SortLotsByAuctionIdAndBookletName(List<Lot> lots)
-//{
-
-//    // Vervolgens sorteren op naam (name) vanuit de class Booklet
-//    List<Lot> sortedLotsByName = sortedLotsByAuctionId.OrderBy(lot => lot.GetBookletId().GetName()).ToList();
-
-//    return sortedLotsByName;
-//}
 
