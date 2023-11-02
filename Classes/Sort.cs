665aeadf94ee1e4ea566ec0105f022c250efc79d
Jeroen Stekelenburg <1747142stekelenburg@zuyd.nl>
2023-11-02 12:34:37 +0100

    AAnpassingen aan de Sort class

--- Classes/Sort.cs
+++ Classes/Sort.cs
@@ -1,23 +1,141 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace MBPC_VeilingApp.Classes
 {
     internal class Sort
     {
+        public class LotSorter : IComparer<List<object>>
+            {
+                public int Compare(List<object> x, List<object> y)
+                {
+                    for (int i = 0; i < Math.Min(x.Count, y.Count); i++)
+                    {
+                        if (x[i] is int && y[i] is int)
+                        {
+                            int numX = (int)x[i];
+                            int numY = (int)y[i];
 
-        public List<Lot> SortLotsByAuctionId(List<Lot> lots, int auctionId)
-        {
-            var sortedLots = lots
-                .Where(lot => lot.AuctionId == auctionId) // Filter op het gewenste auctionId
-                .OrderBy(lot => lot.AuctionId)
-                .ThenBy(lot => lot.LotNumber) // Sorteer binnen de veiling op lotnummer (of andere eigenschap)
-                .ToList();
+                            int result = numX.CompareTo(numY);
 
-            return sortedLots;
+                            if (result != 0)
+                            {
+                                return result;
+                            }
+                        }
+                        else
+                        {
+                            string strX = x[i].ToString();
+                            string strY = y[i].ToString();
+
+                            int result = strX.CompareTo(strY);
+
+                            if (result != 0)
+                            {
+                                return result;
+                            }
+                        }
+                    }
+
+                    return x.Count.CompareTo(y.Count);
+                }
+            }
+
+            public List<Lot> SortLotsByAuctionIdAndBookletName(List<Lot> lots)
+            {
+                List<Lot> sortedLotsByAuctionId = lots.OrderBy(lot => lot.GetAuctionId().GetId()).ToList();
+                List<Lot> sortedLotsByName = sortedLotsByAuctionId.OrderBy(lot =>
+                {
+                    string bookletName = lot.GetBookletId().GetName();
+                    List<string> parts = Regex.Split(bookletName, @"(?<=[0-9])(?=[A-Za-z])").ToList();
+                    List<object> parsedParts = new List<object>();
+
+                    foreach (string part in parts)
+                    {
+                        if (int.TryParse(part, out int number))
+                        {
+                            parsedParts.Add(number);
+                        }
+                        else
+                        {
+                            parsedParts.Add(part);
+                        }
+                    }
+
+                    return parsedParts;
+                }, new LotSorter()).ToList();
+
+                return sortedLotsByName;
+            }
         }
     }
-}
+//    private class CustomComparer : IComparer<List<object>>
+//    {
+//        public int Compare(List<object> x, List<object> y)
+//        {
+//            for (int i = 0; i < Math.Min(x.Count, y.Count); i++)
+//            {
+//                if (x[i] is int && y[i] is int)
+//                {
+//                    int numX = (int)x[i];
+//                    int numY = (int)y[i];
+
+//                    int result = numX.CompareTo(numY);
+
+//                    if (result != 0)
+//                    {
+//                        return result;
+//                    }
+//                }
+//                else
+//                {
+//                    string strX = x[i].ToString();
+//                    string strY = y[i].ToString();
+
+//                    int result = strX.CompareTo(strY);
+
+//                    if (result != 0)
+//                    {
+//                        return result;
+//                    }
+//                }
+//            }
+
+//            return x.Count.CompareTo(y.Count);
+//        }
+//    }
+//    public List<Lot> SortLotsByAuctionIdAndBookletName(List<Lot> lots)
+//    {
+//        List<Lot> sortedLotsByName = lots;
+//        List<Lot> sorted = sortedLotsByName.OrderBy(lot =>
+//        {
+//            string bookletName = lot.GetBookletId().GetName();
+//            List<string> parts = Regex.Split(bookletName, @"(?<=[0-9])(?=[A-Za-z])").ToList();
+//            List<object> parsedParts = new List<object>();
+
+//            foreach (string part in parts)
+//            {
+//                if (int.TryParse(part, out int number))
+//                {
+//                    parsedParts.Add(number);
+//                }
+//                else
+//                {
+//                    parsedParts.Add(part);
+//                }
+//            }
+
+//            return parsedParts;
+//        }, new CustomComparer()).ToList();
+
+//        return sortedLotsByName;
+//    }
+//}
+//public List<Lot> SortLotsByAuctionIdAndBookletName(List<Lot> lots)
+//{
+
+//    // Vervolgens sorteren op naam (name) vanuit de class Booklet
+//    List<Lot> sortedLotsByName = sortedLotsByAuctionId.OrderBy(lot => lot.GetBookletId().GetName()).ToList();
+
+//    return sortedLotsByName;
+//}
+
--- Program.cs
+++ Program.cs
@@ -47,9 +47,9 @@ using System.Data.SqlClient;
 
 //----------------------------------------------------------------------------------------------------> Voorbeeld van Jeroen
 
-//DAL.RefreshDAL()
+DAL.RefreshDAL();
 
-// implementatie komt hier 
+//// implementatie komt hier 
 //Lot lot = new Lot(0, DAL.auctions.First(), DAL.members.First(), DAL.booklets.First(), 0, "", "", "", 0, 0, "");
 
 //try
@@ -64,6 +64,19 @@ using System.Data.SqlClient;
 //lot.CreateLot();
 
 //Console.WriteLine(Lot.ReadLot);
+// Eerst de lots ophalen uit je DAL of ergens anders
+
+List<Lot> lots = Lot.ReadLot().Where(l => l.GetAuctionId().GetId() == 1).ToList(); // Verondersteld dat je de ReadLot-methode hebt toegevoegd in de klasse
+
+// De LotSorter-klasse gebruiken om de lots te sorteren op auctionId en daarna op naam (name)
+Sort lotSorter = new Sort();
+List<Lot> sortedLots = lotSorter.SortLotsByAuctionIdAndBookletName(lots);
+
+foreach (Lot l in sortedLots)
+{
+    Console.WriteLine($"LotId:{l.GetId()} Name: {l.GetBookletId().GetName()}");
+}
+//Nu heb je de gesorteerde lots in de 'sortedLots'-lijst, gesorteerd op auctionId en vervolgens op naam.
 //----------------------------------------------------------------------------------------------------> Voorbeeld van Jeroen
 
 //----------------------------------------------------------------------------------------------------> Voorbeeld van Kane
